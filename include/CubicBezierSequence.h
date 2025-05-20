f91ed29b26cd9c16fb501776dc9148cc390db8db
Matos <18725330+tosmatos@users.noreply.github.com>
2025-05-20 14:26:15 +0200

    Adds CubicBezierSequence for continuity

---
+++ include/CubicBezierSequence.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Bezier.h"
+#include "Vertex.h"
+
+#include <vector>
+
+class CubicBezierSequence {
+private:
+    std::vector<Bezier> curves;
+    int continuityType = 0; // 0 for C0, 1 for C1, 2 for C2
+
+public:
+    // Add a new curve to the sequence
+    void addCurve(const Bezier& curve);
+
+    // Enforce continuity constraints on all curves
+    void enforceConstraints();
+
+    // Set the continuity type (0=C0, 1=C1, 2=C2)
+    void setContinuityType(int type);
+
+    // Move a control point, maintaining continuity
+    void moveControlPoint(int curveIndex, int pointIndex, const Vertex& newPosition);
+
+    // Draw all curves
+    void draw(Shader& shader);
+};
---
+++ src/CubicBezierSequence.cpp
@@ -0,0 +1,21 @@
+#include "CubicBezierSequence.h"
+
+void CubicBezierSequence::addCurve(const Bezier& curve)
+{
+}
+
+void CubicBezierSequence::enforceConstraints()
+{
+}
+
+void CubicBezierSequence::setContinuityType(int type)
+{
+}
+
+void CubicBezierSequence::moveControlPoint(int curveIndex, int pointIndex, const Vertex& newPosition)
+{
+}
+
+void CubicBezierSequence::draw(Shader& shader)
+{
+}
